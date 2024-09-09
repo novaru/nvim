1910cfc2e947fdf80c8e667a94f69e574822b728
Febriyan Ahza <69071485+novaru@users.noreply.github.com>
2024-09-09 10:37:31 +0700

    style: tidying up the folders and reformatting

--- lua/myinit.lua
+++ lua/myinit.lua
@@ -2,10 +2,7 @@ local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
 vim.opt.rtp:prepend(lazypath)
 
--- local autocmd = vim.api.nvim_create_autocmd
-
--- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
+-- vim.o.tabstop = 4
+-- vim.o.expandtab = true
+-- vim.o.softtabstop = 4
+-- vim.o.shiftwidth = 4
